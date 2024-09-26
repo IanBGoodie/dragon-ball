ea3f6c20174742aac62433a886ef710a7be914f1
IE Dev 6 <ie.dev.6@hotmail.com>
2024-09-26 16:04:34 -0700

    26 de sep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonajesComponent } from "./personajes/personajes.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonajesComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dragon-ball';
+  title = 'The Dragon Ball API';
 }
